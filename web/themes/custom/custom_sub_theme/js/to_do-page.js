101fc8ed22eedbf0da7319654ff31357b66bab53
sourabhInno <sourabh.sisodia@innoraft.com>
2023-09-28 11:19:13 +0530

    FT2023-417: Made a to do list with add and remove buttons

--- web/themes/custom/custom_sub_theme/js/to_do-page.js
+++ web/themes/custom/custom_sub_theme/js/to_do-page.js
@@ -1,8 +1,32 @@
-(function ($) {
-  Drupal.behaviors.myModuleBehavior = {
+(function ($, Drupal, once) {
+  Drupal.behaviors.toDoList = {
     attach: function (context, settings) {
+      once("toDo", "#toDoContainer", context).forEach((element) => {
+        const addButton = document.querySelector("#add");
+        const inputText = document.querySelector("#new-item");
+        const taskList = document.querySelector("#mylist");
 
-      console.log("hii");
-    }
+        let remove = (e) => {
+          let element = e.target;
+          element.parentNode.remove();
+        };
+        addButton.onclick = () => {
+          const value = inputText.value;
+          if (value !== "") {
+            const li = document.createElement("li");
+            li.classList.add("list-group-item");
+            const removeButton = document.createElement("div");
+            removeButton.classList.add("text-right", "btn", "btn-danger");
+            const removeButtonText = document.createTextNode("X");
+            removeButton.appendChild(removeButtonText);
+            removeButton.onclick = remove;
+            const text = document.createTextNode(value);
+            li.appendChild(text);
+            li.appendChild(removeButton);
+            taskList.appendChild(li);
+          }
+        };
+      });
+    },
   };
-})(jQuery);
+})(jQuery, Drupal, once);
